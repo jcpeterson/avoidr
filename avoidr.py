57ff0f8c246b509e05ba3ad18378ae1539f682fb
jcpeterson <peterson.c.joshua@gmail.com>
2014-07-06 02:40:52 -0700

    created main script with window initialization

---
+++ avoidr.py
@@ -0,0 +1,18 @@
+# import libraries
+import pygame
+import time, math
+
+# size of the window
+(width, height) = (800, 600)
+
+# create a new black window
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('avoidr.v0.01')
+background_colour = (0,0,0)
+screen.fill(background_colour)
+
+running = True
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
