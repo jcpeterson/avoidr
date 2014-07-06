a578cc2a82bcb442d07761d44fa089d816df968a
Joshua Peterson <peterson.c.joshua@gmail.com>
2014-07-06 14:21:48 -0700

    added player movement
    v0.02

--- avoidr.py
+++ avoidr.py
@@ -2,19 +2,58 @@
 import pygame
 import time, math
 
-# size of the window
-(width, height) = (800, 600)
+# size of the window in pixels
+# this should be automatic in the future
+width = 800
+height = 600
 
-# create a new black window
+# create a new window
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('avoidr.v0.01')
-background_colour = (0,0,0)
+# set the window caption
+pygame.display.set_caption('avoidr.v0.02')
+# make a 3-tuple with the color white
+background_colour = (255,255,255)
+# fill the screen with the background color
 screen.fill(background_colour)
-
-running = True
-while running:
+# draw the current frame
+pygame.display.flip()
+# player position
+playerX = 400
+playerY = 300
+# the main game loop
+gameRunning = True
+while gameRunning:
+	# fill the screen with the background color
+	screen.fill(background_colour)
+	# draw the main character
+	pygame.draw.circle(screen, (0,0,0), (playerX, playerY), 25, 0)
+	pygame.display.flip()
+	# handle input
 	for event in pygame.event.get():
+		# exit if X button is pushed
 		if event.type == pygame.QUIT:
-			running = False
-
-#This is Connor.
+			gameRunning = False
+			pygame.QUIT
+		# exit if ESC key is pushed
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				gameRunning = False
+			 	pygame.QUIT
+			# if event.key == pygame.K_UP:
+			# 	playerY = playerY - 10
+			# if event.key == pygame.K_DOWN:
+			# 	playerY = playerY + 10
+			# if event.key == pygame.K_LEFT:
+			# 	playerX = playerX - 10
+			# if event.key == pygame.K_RIGHT:
+			# 	playerX = playerX + 10
+	keys = pygame.key.get_pressed()
+	if keys[pygame.K_UP]:
+		playerY = playerY - 1
+	if keys[pygame.K_DOWN]:
+		playerY = playerY + 1
+	if keys[pygame.K_LEFT]:
+		playerX = playerX - 1
+	if keys[pygame.K_RIGHT]:
+		playerX = playerX + 1
+	time.sleep(0.01)
